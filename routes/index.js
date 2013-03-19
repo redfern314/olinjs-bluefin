147b77a96d9f9ec3fed1ebc3b56c345c648824b2
Reyner Crosby <reyner@gmail.com>
2013-03-18 20:10:36 -0400

    initial foursquare trending venues functionality added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
+var request = require('request');
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var foursquareQuery = "https://api.foursquare.com/v2/venues/trending?ll=37.7955,-122.3937&oauth_token="+process.env.FOURSQUARE_KEY;
+  request({url: foursquareQuery, json:true}, function(error, response, data){
+    res.render('index', {title: "Welcome to San Francisco!", trending: response.body.response.venues});
+  });
 };
