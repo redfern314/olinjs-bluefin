a719acf7b77369a8f8784504445cb150c1e6996c
Derek Redfern <redfern.derek@gmail.com>
2013-03-26 14:33:19 -0400

    Mark restraunts in the area with a pin

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,4 @@
-function initialize() {
+/*function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(-34.397, 150.644),
     zoom: 8,
@@ -7,4 +7,48 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
 }
+google.maps.event.addDomListener(window, 'load', initialize);*/
+
+var map;
+var service;
+var infowindow;
+
+function initialize() {
+  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: pyrmont,
+      zoom: 15
+    });
+
+  var request = {
+    location: pyrmont,
+    radius: '5000',
+    query: 'restaurant'
+  };
+
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  console.log(place);
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    title: place.name,
+    snippet: "lorem ipsum"
+  });
+}
