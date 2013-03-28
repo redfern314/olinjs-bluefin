5a9c132b28926bd3811b78cc37e738fd95ddb08b
Derek Redfern <redfern.derek@gmail.com>
2013-03-27 23:42:05 -0400

    Google Map now pulls results from the 4sq API

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var request = require('request');
  */
 
 exports.index = function(req, res){
+  if(!req.session.location){
+    req.session.location = {lat:37.7955,lng:-122.3937};
+  }
   var foursquareQuery = "https://api.foursquare.com/v2/venues/trending?ll=37.7955,-122.3937&oauth_token="+process.env.FOURSQUARE_KEY;
   request({url: foursquareQuery, json:true}, function(error, foursquareResponse, foursquareData){
   	  if (!error) {
--- routes/map.js
+++ routes/map.js
@@ -3,10 +3,14 @@
  * GET map testing page.
  */
 
-var http = require('http');
+var http = require('http')
+  , request = require('request');
 
 exports.map = function(req, res){
   console.log(process.env.GOOGLE_KEY);
+  if(!req.session.location){
+    req.session.location = {lat:37.7955,lng:-122.3937};
+  }
   res.render('map', { title: 'Map Test', GOOGLE_KEY: process.env.GOOGLE_KEY });
 };
 
@@ -28,5 +32,17 @@ exports.getLocation = function(req, res) {
     });
   }).on('error', function(e) {
     console.log("Got error: " + e.message);
+  });
+}
+
+exports.getVenues = function(req,res) {
+  console.log(req.session.location)
+  var foursquareQuery = "https://api.foursquare.com/v2/venues/trending?ll="+req.session.location.lat+","+req.session.location.lng+"&oauth_token="+process.env.FOURSQUARE_KEY;
+  request({url: foursquareQuery, json:true}, function(error, foursquareResponse, foursquareData){
+      if (!error) {
+        console.log(foursquareResponse.body.response.venues);
+        res.send({trending: foursquareResponse.body.response.venues});
+        //res.render('index', {title: "Welcome to San Francisco!", city : "San Francisco", trending: foursquareResponse.body.response.venues});
+      }
   });
 }
