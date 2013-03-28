03aa891fadbeaf9229d5fc58d903422e656167da
Reyner Crosby <reyner@gmail.com>
2013-03-27 22:38:20 -0400

    tweets render in the thing

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ exports.index = function(req, res){
   var foursquareQuery = "https://api.foursquare.com/v2/venues/trending?ll=37.7955,-122.3937&oauth_token="+process.env.FOURSQUARE_KEY;
   request({url: foursquareQuery, json:true}, function(error, foursquareResponse, foursquareData){
   	  if (!error) {
+        console.log(foursquareResponse.body.response.venues);
    	  	res.render('index', {title: "Welcome to San Francisco!", city : "San Francisco", trending: foursquareResponse.body.response.venues});
   	  }
   });
